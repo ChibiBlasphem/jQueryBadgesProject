8cd2c81ff5169938a637e499fc4148abc5587c79
ChibiBlasphem <chibiblasphem@gmail.com>
2016-12-13 00:47:10 +0100

    build step

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
-  // your code will go here
+  // Ajax query
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Chibiblasphem.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var $badges = $('#badges');
+
+      response.courses.completed.forEach(function(course) {
+        var $div = $('<div>').addClass('course');
+        var $h3 = $('<h3>').text(course.title);
+        var $img = $('<img>').attr('src', course.badge);
+        var $a = $('<a>').attr('href', course.url).attr('target', '_blank');
+
+        $div.append($h3).append($img).append($a);
+        $badges.append($div);
+      });
+    }
+  });
 
 });
