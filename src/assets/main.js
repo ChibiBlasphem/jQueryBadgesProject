04ebce87f75a18f6d43501fc9274624a5ff04e59
ChibiBlasphem <chibiblasphem@gmail.com>
2016-12-13 00:49:04 +0100

    Add classes and textContent for anchor tag

--- src/assets/main.js
+++ src/assets/main.js
@@ -11,7 +11,7 @@ $(function() {
         var $div = $('<div>').addClass('course');
         var $h3 = $('<h3>').text(course.title);
         var $img = $('<img>').attr('src', course.badge);
-        var $a = $('<a>').attr('href', course.url).attr('target', '_blank');
+        var $a = $('<a>').attr('href', course.url).attr('target', '_blank').addClass('btn btn-primary').text('See Course');
 
         $div.append($h3).append($img).append($a);
         $badges.append($div);
